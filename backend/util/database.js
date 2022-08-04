ed0016d20fff01b91d2fb0c4e8a4385a06002892
Harshul Rai <harshuljps@gmail.com>
2022-08-04 15:56:28 +0530

    task 5

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize=new Sequelize('node-complete','root','965636@Leb',{
+const sequelize=new Sequelize('node-complete','root','123@Hello',{
     dialect:'mysql',
     host: 'localhost'
 })
